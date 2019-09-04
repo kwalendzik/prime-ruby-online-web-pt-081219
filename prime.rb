8b81b85e5f89e150f5dfc7343dfaf446732f3f16
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-09-04 23:58:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,9 @@
-prime?(num)
-  if num < 2
-    return false
-  else 
+def prime? (n)
+    if n <= 1
+        false
+    elsif n == 2
+        true
+    else 
+        (2..n/2).none? { |i| n % i == 0}
+    end
+end
